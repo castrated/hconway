89c1039ddc7b0c3d3850706c05e5550ec53ac610
castrated <47220664+castrated@users.noreply.github.com>
2019-10-16 11:43:52 +0000

    i don't like non-functional whitespace

--- hconway.s
+++ hconway.s
@@ -1,12 +1,10 @@
 	call a.memorization
 	call mn_num
 	int3
-
 a.memorization:
 	mov [a+1*4],1
 	mov [a+2*4],1
 	mov ecx,3
-
 @@:
 	mov eax,ecx
 	mov edx.[a+(ecx-1)*4]
@@ -18,9 +16,7 @@ a.memorization:
 	cmp ecx,1 shl 20
 	jnz @B
 	retn
-
 _0.55 equ ((55 shl 32)/100) ; FF[55 * 2^N / 100], for N=32
-
 mn_num:
 	mov ecx,1 shl 20
 @@:	dec ecx
@@ -30,5 +26,4 @@ mn_num:
 	cmp eax,_0.55 + 1
 	jc @B
 	retn
-
 a rd 1 shl 20
